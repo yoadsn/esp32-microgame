8a4f3acef5acbc036456dba7b094ada9bbc94da1
Yoad <yoad@wescover.com>
2024-03-13 11:10:30 +0200

    Power/Charge bars

--- games/duel_game.py
+++ games/duel_game.py
@@ -2,6 +2,10 @@ import random
 from game_device import GameDevice
 from game_logic import BaseGameLogic
 
+FIELD_WIDTH = 116
+
+PLAYER_POSITION_TOP = 0
+PLAYER_POSITION_BOTTOM = 1
 
 PST_INIT = -1
 PST_DEFENSIVE = 0
@@ -13,11 +17,12 @@ PST_EXPLODED = 4
 STOP_TO_CHARGE_WAIT_MS = 10
 CHARGE_TO_FIRE_WAIT_PER_POWER_MS = 55
 PLAYER_INITIAL_SPEED_PX_F = 3
-PLAYER_BASE_POWER_POINTS = 20
+PLAYER_BASE_POWER_POINTS = 10
 PLAYER_BASE_LENGTH_PX = 40
 PLAYER_MIN_BASE_LENGTH_PX = 5
 
 PROJECTILE_BLAST_RADIUS = 2
+PROJECTILE_SPEED = 2
 
 
 class Projectile:
@@ -25,10 +30,10 @@ class Projectile:
         self.display = display
         self.x = x
         self.y = y
-        self.direction = direction  # 1 for downwards, -1 for upwards
+        self.direction = direction
 
     def move(self):
-        self.y += self.direction  # Move the projectile 1 pixel per 5 frames
+        self.y += self.direction * PROJECTILE_SPEED
 
     def draw(self):
         self.display.pixel(int(self.x), int(self.y), 1)
@@ -42,32 +47,192 @@ class Projectile:
         )
 
 
+BAR_FILL_DIRECTION_TTB = 1
+BAR_FILL_DIRECTION_BTT = -1
+
+
+class ProgressBar:
+    def __init__(
+        self,
+        display,
+        fill_direction: int,  # 1 with orientation -1 again
+        rect,  # bounds of the bar
+        show_max_line: bool = False,
+    ):
+        self.display = display
+        self.fill_direction = fill_direction
+        rect_x1, rect_y1, rect_w, rect_h = rect
+        self.height = self.max_height = rect_h
+        self.start_x = rect_x1
+        self.width = rect_w
+        # depends on fill direction
+        self.start_y = (
+            rect_y1 if fill_direction == BAR_FILL_DIRECTION_TTB else rect_y1 + rect_h
+        )
+        self.show_max_line = show_max_line
+        if show_max_line:
+            self.set_max_height(self.max_height)
+
+    def set_max_height(self, new_max_height):
+        self.max_height = new_max_height
+        self.max_line_y = int(
+            self.start_y + self.max_height - 1
+            if self.fill_direction == BAR_FILL_DIRECTION_TTB
+            else self.start_y - self.max_height + 1
+        )
+
+    def draw(self, fill):
+        fill_height = fill
+        self.display.fill_rect(
+            self.start_x,
+            (
+                self.start_y
+                if self.fill_direction == BAR_FILL_DIRECTION_TTB
+                else self.start_y - fill_height
+            ),
+            self.width,
+            fill_height,
+            1,
+        )
+
+        if self.show_max_line:
+            self.display.line(
+                self.start_x,
+                self.max_line_y,
+                self.start_x + self.width - 1,
+                self.max_line_y,
+                1,
+            )
+
+
+class ChargeBar(ProgressBar):
+    def __init__(
+        self,
+        display,
+        fill_direction: int,
+        rect,
+    ):
+        super().__init__(display, fill_direction, rect, True)
+        self.full_charge_pct = 1
+
+    def set_full_charge_pct(self, max_charge_pct):
+        self.full_charge_pct = max_charge_pct
+        super().set_max_height(self.height * self.full_charge_pct)
+
+    def draw(self, pct):
+        super().draw(int(self.height * pct * self.full_charge_pct))
+
+
+class PowerBar(ProgressBar):
+    def __init__(self, display, fill_direction: int, rect, max_power: int):
+        self.max_power = max_power
+        super().__init__(display, fill_direction, rect, False)
+
+    def draw(self, power):
+        super().draw(int(self.height * power / self.max_power))
+
+
 class Player:
-    def __init__(self, time, display, power_points, position="top", initialSpeed=3):
+    def __init__(
+        self,
+        time,
+        display,
+        field_start: int,
+        field_end: int,
+        bezel_start: int,
+        bezel_end: int,
+        power_points: int,
+        position: int = PLAYER_POSITION_TOP,
+        initialSpeed: int = 3,
+    ):
         # Game access
         self.time = time
         self.display = display
 
+        # Game Setup
+        self.field_start = field_start
+        self.field_end = field_end
+        # May change in-game - so dynamic in theory
+        self.field_width = field_end - field_start
+        self.bezel_start = bezel_start
+        self.bezel_end = bezel_end
+        self.bezel_width = bezel_end - bezel_start
+
+        # Positioning
+        self.position = position
+        self.direction = 1 - self.position * 2  # 1 or -1
+        self.x = self.field_width // 2
+        self.y = 1 if position == PLAYER_POSITION_TOP else display.height - 2
+
         # State initialization
         self.play_state: int = PST_INIT
         self.play_state_start: int = 0
-
         self.power_points = power_points
-        self.position = position
 
-        self.x = display.width // 2
-        self.y = 1 if position == "top" else display.height - 2
+        self.sprite = display.get_buffer(
+            bytearray(
+                (
+                    0b0000_0000,
+                    0b0000_0000,
+                    0b0011_1100,
+                    0b0011_1100,
+                    0b1110_1110,
+                    0b0111_0111,
+                    0b0000_0011,
+                    0b1100_0000,
+                    0b0000_0001,
+                    0b1000_0000,
+                )
+            ),
+            16,
+            5,
+        )
+
         self.vx = initialSpeed
-        self.building_cannon = False
-        self.reverting_to_defensive = False
-        self.cannon_height = 0
         self.projectile = None
+        self.charge_pct = 0
+
+        # Charge / Power Bars
+        bar_height = display.height // 2
+        bar_width = self.bezel_width
+        bar_direction = (
+            BAR_FILL_DIRECTION_TTB
+            if position == PLAYER_POSITION_TOP
+            else BAR_FILL_DIRECTION_BTT
+        )
+        bar_rect = (
+            (
+                bezel_start,  # charge bar start x
+                0,
+                bar_width,
+                bar_height,
+            )
+            if position == PLAYER_POSITION_TOP
+            else (
+                bezel_start,  # charge bar start x
+                display.height - bar_height,
+                bar_width,
+                bar_height,
+            )
+        )
+        self.charge_bar = ChargeBar(
+            display,
+            bar_direction,
+            bar_rect,
+        )
+        self.power_bar = PowerBar(
+            display,
+            bar_direction,
+            bar_rect,
+            power_points,
+        )
 
     def play(self, button):
         # progress state machine
         curr_state = self.play_state
         next_state = PST_DEFENSIVE
         now = self.time.ticks_ms()
+        self.charge_pct = 0
 
         if self.power_points == 0:
             next_state = PST_EXPLODED
@@ -80,7 +245,6 @@ class Player:
                     next_state = PST_STOPPED
                     if time_in_state > STOP_TO_CHARGE_WAIT_MS and not self.projectile:
                         next_state = PST_CHARGING
-                        self.charge_pct = 0
                 elif curr_state == PST_CHARGING:
                     next_state = PST_CHARGING
                     self.charge_pct = time_in_state / float(
@@ -100,19 +264,17 @@ class Player:
 
         # Act on state
         if self.play_state == PST_FIRING:
-            direction = 1 if self.position == "top" else -1
             self.projectile = Projectile(
                 self.display,
                 self.x,
                 self.y,
-                direction,
+                self.direction,  # go up (pos 1) or down (pos 0)
             )
-            self.power_points -= 1  # Lose one power point after shooting
 
     def check_hit(self, rect):
         if self.play_state == PST_EXPLODED:
             return False
-        
+
         x1, y1, x2, y2 = rect
         # Why the ugly nested if? To save on calcs
         if y1 <= self.y and y2 >= self.y:
@@ -124,8 +286,16 @@ class Player:
                     return True
         return False
 
+    def update_power(self, power_diff):
+        self.power_points += power_diff
+        self.charge_bar.set_full_charge_pct(
+            self.power_points / PLAYER_BASE_POWER_POINTS
+        )
+
     def move(self):
-        width = self.display.width
+        field_start = self.field_start
+        field_end = self.field_end
+        width = self.field_width
         height = self.display.height
         curr_state = self.play_state
 
@@ -136,7 +306,10 @@ class Player:
         if curr_state == PST_DEFENSIVE:
             # Normal defensive movement logic
             self.x += self.vx
-            if self.x <= player_half_width or self.x >= width - player_half_width:
+            if (
+                self.x <= field_start + player_half_width
+                or self.x >= field_end - player_half_width
+            ):
                 self.vx *= -1
                 self.x = self.x
 
@@ -157,31 +330,25 @@ class Player:
             display.rect(x, y, 1, 1, 1)
         else:
             player_half_width = self.player_width // 2
+            """ display.blit(
+                self.sprite,
+                self.x - 8,
+                self.y + (0 if self.position == PLAYER_POSITION_TOP else -5),
+            ) """
             display.line(
                 int(x - player_half_width), y, int(x + player_half_width), y, 1
-            )
-
-        # Draw the cannon shape
-        if curr_state == PST_CHARGING:
-            charge_bar_y = y + (1 if self.position == "top" else -1)
-            display.line(
-                int(x - player_half_width),
-                charge_bar_y,
-                int(x - player_half_width + player_half_width * self.charge_pct),
-                charge_bar_y,
-                1,
-            )
-            display.line(
-                int(x + player_half_width),
-                charge_bar_y,
-                int(x + player_half_width - player_half_width * self.charge_pct),
-                charge_bar_y,
-                1,
             )
 
         # Draw the projectile
         if self.projectile:
             self.projectile.draw()
+
+        if curr_state == PST_CHARGING:
+            # Draw Charging progress
+            self.charge_bar.draw(self.charge_pct)
+        else:
+            # Draw power
+            self.power_bar.draw(self.power_points)
 
 
 class ComputerController:
@@ -213,30 +380,47 @@ class GameLogic(BaseGameLogic):
         print("game loaded")
         self.count_down_to_invert = 0
         self.start_game_tick = self.device.time.ticks_ms()
-        self.hit_mode = False
+
+        self.field_width = FIELD_WIDTH
+        self.field_start = (self.device.display.width - self.field_width) // 2
+        self.field_end = self.device.display.width - self.field_start
+
         self.top_player = Player(
             self.device.time,
             self.device.display,
-            position="top",
+            self.field_start,
+            self.field_end,
+            0,  # bezel
+            self.field_start,  # bezel
+            position=PLAYER_POSITION_TOP,
             power_points=PLAYER_BASE_POWER_POINTS,
             initialSpeed=PLAYER_INITIAL_SPEED_PX_F,
         )
         self.bottom_player = Player(
             self.device.time,
             self.device.display,
-            position="bottom",
+            self.field_start,
+            self.field_end,
+            self.field_end,  # bezel
+            self.device.display.width,  # bezel
+            position=PLAYER_POSITION_BOTTOM,
             power_points=PLAYER_BASE_POWER_POINTS,
             initialSpeed=PLAYER_INITIAL_SPEED_PX_F,
         )
         self.npc = ComputerController(self.bottom_player)
 
+    def draw(self):
+        self.device.display.rect(
+            self.field_start, 0, self.field_width, self.device.display.height, 1
+        )
+
     def hit(self, shooter: Player, target: Player):
         if shooter.projectile:
             proj_rect = shooter.projectile.get_hit_rect()
             if target.check_hit(proj_rect):
-                target.power_points -= 1
+                target.update_power(-1)
                 shooter.projectile = None  # Can't hit again!
-                self.hit_mode = True
+                self.count_down_to_invert = 5
 
     def game_tick(self):
         device = self.device
@@ -261,14 +445,12 @@ class GameLogic(BaseGameLogic):
         # Clear display and redraw players
         display.fill(0)
 
+        self.draw()
         self.top_player.draw()
         self.bottom_player.draw()
 
-        if self.hit_mode:
-            self.hit_mode = False
-            self.count_down_to_invert = 5
+        if self.count_down_to_invert > 0:
             display.invert(1)
-        elif self.count_down_to_invert > 0:
             self.count_down_to_invert -= 1
             if self.count_down_to_invert == 0:
                 display.invert(0)
